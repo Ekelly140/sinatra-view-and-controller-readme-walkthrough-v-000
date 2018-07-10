0d064287d66b80d2c3a0f92532725b126ec38e32
ERIC KELLY <eakelly220@gmail.com>
2018-07-10 19:42:43 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,9 +8,7 @@ class App < Sinatra::Base
     erb :reversed
   end
 
-  post '/reverse' do
-    erb :reversed
-  end
+
 
   get '/friends' do
     @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
